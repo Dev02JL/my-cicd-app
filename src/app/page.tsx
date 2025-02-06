d3379d249474f7cf8f24998004d3e8201591409d
Jean-Louis <dev02@sportyneo.com>
2025-02-06 14:45:21 +0100

    Workflow GitHub Actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <GradientTitle>My Ansible TodoList</GradientTitle>
+      <GradientTitle>My GitHub Actions TodoList</GradientTitle>
       <TodoList />
        
       {/* Bouton pour aller au Carousel */}
