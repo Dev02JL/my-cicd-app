354e49dbdf4cc0d34e00c85e2dcfbd54599d09ae
Jean-Louis <dev02@sportyneo.com>
2025-02-06 15:20:41 +0100

    Fix Wording

--- src/app/carousel/page.tsx
+++ src/app/carousel/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function CarouselPage() {
   return (
     <div className="flex flex-col items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <GradientTitle>My Actions Carousel</GradientTitle>
+      <GradientTitle>My Github Carousel</GradientTitle>
       <ImageCarousel />
 
       {/* Bouton Retour à la TodoList */}
